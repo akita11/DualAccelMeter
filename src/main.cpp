94c1f138e8992dfe01f713e4f334f2cbf4d5e4b7
Junichi Akita <akita@ifdl.jp>
2024-12-18 21:23:12 +0900

    tentative init code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <M5Unified.h>
 #include <Ticker.h>
+#include "bmi270_config.h"
 
 // IMU Unit
 // https://www.switch-science.com/products/6623
@@ -54,10 +55,38 @@ void setup()
 
   M5.Display.setCursor(10, 30);
 	M5.Display.printf("IMU test");
+
+//	static constexpr const std::uint8_t INIT_ADDR_0             = 0x5B;
+//	static constexpr const std::uint8_t INIT_ADDR_1             = 0x5C;
+//	static constexpr const std::uint8_t INIT_DATA_ADDR          = 0x5E;
+
+	uint8_t index = 0;
+  uint8_t addr_array[2] = {
+      (uint8_t)((index >> 1) & 0x0F),
+      (uint8_t)(index >> 5)
+    };
+
+//	M5.Ex_I2C.writeRegister( I2C_ADDR1, 0x5b, addr_array, 2, I2C_CLK_FREQ_MAX);
+//  M5.Ex_I2C.writeRegister( I2C_ADDR1, 0x5e, (uint8_t *)bmi270_config_file, sizeof(bmi270_config_file), I2C_CLK_FREQ_MAX);
+
+	// IMU init sequence
+	// write_reg(0x7c, 0x00); // disable adv.power save
+	// wait 450us
+	// write_reg(0x59, 0x00); // prepare init
+	// prepare init_array[] & burst_write from 0x5e
+	// write_reg(0x59, 0x01); // complete init
+
+	// read_reg(0x21) == 1 -> OK
+	// write_reg(0x7d, 0x04); enable acc
+	// write_reg(0x40, 0xaa); // ODR=400Hz, bwp=normal, filer=performance opt. 
+	// write_reg(0x7c, 0x02); // disable adv. power save, enable fifo wakeup
+	// read acc
+
 }
 
 void loop()
 {
+	// CHIP_ID(0x00) = 0x24
   printf("%02x\n", M5.Ex_I2C.readRegister8(I2C_ADDR1, 0x00, I2C_CLK_FREQ_MAX));
 	delay(1000);
 //  ticker.attach_ms((int)(1000 / SAMPLE_FREQ), onTicker);
