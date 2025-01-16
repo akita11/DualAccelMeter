6e7a56fb79612078bdc45722df1961fb9324faa0
Junichi Akita <akita@ifdl.jp>
2025-01-17 08:58:20 +0900

    measure@250Hz

--- src/main.cpp
+++ src/main.cpp
@@ -26,27 +26,7 @@ float conv_acc(uint8_t dh, uint8_t dl){
 Ticker ticker;
 uint8_t fRun = 0;
 uint32_t w = 0;
-uint8_t buf[10];
-
-void IRAM_ATTR onTicker()
-{
-  if (fRun == 1)
-  {
-    w++;
-    if (w == SAMPLE_FREQ)
-    { // display every 1 sec
-      w = 0;
-    }
-  }
-}
-
-void drawStatus(uint8_t f)
-{
-  if (f == 1)
-    M5.Display.fillRect(230, 0, 240, 240, RED);
-  else
-    M5.Display.fillRect(230, 0, 240, 240, GREEN);
-}
+uint8_t buf0[10], buf1[10];
 
 #define writeRegB(i2c_addr, reg_addr, data) M5.Ex_I2C.writeRegister8(i2c_addr, reg_addr, data, I2C_CLK_FREQ)
 #define writeReg(i2c_addr, reg_addr, data, len) M5.Ex_I2C.writeRegister(i2c_addr, reg_addr, data, len, I2C_CLK_FREQ)
@@ -56,7 +36,7 @@ void drawStatus(uint8_t f)
 void IMUinit(uint8_t i2c_addr)
 {
 	uint8_t index = 0;
-  uint8_t addr_array[2] = { (uint8_t)((index >> 1) & 0x0F), (uint8_t)(index >> 5) };
+	uint8_t addr_array[2] = { (uint8_t)((index >> 1) & 0x0F), (uint8_t)(index >> 5) };
 
 	// IMU init sequence
 //	printf("CHIP_ID(%02x) : %02x\n", readRegB(I2C_ADDR_IMU1, 0x00)); // CHIP_ID(0x00) = 0x24
@@ -65,7 +45,7 @@ void IMUinit(uint8_t i2c_addr)
 	writeRegB(i2c_addr, 0x59, 0x00); // prepare init
 
 	writeReg(i2c_addr, 0x5b, addr_array, 2);
-  writeReg(i2c_addr, 0x5e, (uint8_t *)bmi270_config_file, sizeof(bmi270_config_file));
+	writeReg(i2c_addr, 0x5e, (uint8_t *)bmi270_config_file, sizeof(bmi270_config_file));
 	writeRegB(i2c_addr, 0x59, 0x01);
 	while(readRegB(i2c_addr, 0x21) != 0x01); // INTERNAL_STATUS(0x21), 0x01=init ok
 
@@ -75,42 +55,83 @@ void IMUinit(uint8_t i2c_addr)
 	writeRegB(i2c_addr, 0x41, 0x00); // range : +-2g
 }
 
+uint32_t t0, tm;
+
+void IRAM_ATTR onTicker()
+{
+		readReg(I2C_ADDR_IMU0, 0x0c, buf0, 6); // accxL, accxH, accyL, accyH, acczL, acczH
+		readReg(I2C_ADDR_IMU1, 0x0c, buf1, 6); // accxL, accxH, accyL, accyH, acczL, acczH
+		ax[0] = conv_acc(buf0[1], buf0[0]);
+		ay[0] = conv_acc(buf0[3], buf0[2]);
+		az[0] = conv_acc(buf0[5], buf0[4]);
+		ax[1] = conv_acc(buf1[1], buf1[0]);
+		ay[1] = conv_acc(buf1[3], buf1[2]);
+		az[1] = conv_acc(buf1[5], buf1[4]);
+		uint32_t t1 = micros();
+		tm = t1 - t0;
+		t0 = t1;
+		printf("%.3f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n", (double)tm/1000.0, ax[0], ay[0], az[0], ax[1], ay[1], az[1]);
+		w++;
+		if (w == SAMPLE_FREQ)
+		{ // display every 1 sec
+/*
+			M5.Display.fillRect(0, 20, 240, 220, BLACK);
+			M5.Display.setCursor(0, 20);
+			M5.Display.printf("#0: %.2f %.2f %.2f\n", ax[0], ay[0], az[0]);
+			M5.Display.setCursor(0, 40);
+			M5.Display.printf("#0: %.2f %.2f %.2f\n", ax[1], ay[1], az[1]);
+	*/
+			w = 0;
+		}
+}
+
+void setMeasure(uint8_t f)
+{
+	if (f == 1){
+		M5.Display.fillRect(230, 0, 240, 240, GREEN);
+	  ticker.attach_ms((int)(1000 / SAMPLE_FREQ), onTicker);
+	}
+	else{
+ 		ticker.detach();
+		M5.Display.fillRect(230, 0, 240, 240, RED);
+	}
+}
+
 void setup()
 {
-  auto cfg = M5.config();
-  cfg.external_imu = false; // for local IMU instalces
-  cfg.internal_imu = false;
-  cfg.internal_spk = false;
-  cfg.internal_mic = false;
+	auto cfg = M5.config();
+	cfg.external_imu = false;
+	cfg.internal_imu = false;
+	cfg.internal_spk = false;
+	cfg.internal_mic = false;
 
-  // I2C clock is defined in Unified/src/utility/imu/IMU_Base.hpp
-  M5.begin(cfg);
-  M5.Ex_I2C.begin();
-  M5.Lcd.setFont(&fonts::DejaVu24);
+	// I2C clock is defined in Unified/src/utility/imu/IMU_Base.hpp
+	M5.begin(cfg);
+	M5.Ex_I2C.begin();
+	M5.Display.setFont(&fonts::DejaVu24);
 
-  M5.Display.setCursor(0, 0); M5.Display.printf("IMU init #0...");
+	M5.Display.setCursor(0, 0); M5.Display.printf("IMU init #0...");
 	IMUinit(I2C_ADDR_IMU0);
-  M5.Display.setCursor(0, 0); M5.Display.printf("IMU init #1...");
+	M5.Display.setCursor(0, 0); M5.Display.printf("IMU init #1...");
 	IMUinit(I2C_ADDR_IMU1);
 
 	M5.Display.clear();
-  M5.Display.setCursor(0, 0);
-	M5.Display.printf("IMU test");
+	M5.Display.setCursor(0, 0);
+	M5.Display.printf("Dual IMU @ 250Hz");
 
-//  ticker.attach_ms((int)(1000 / SAMPLE_FREQ), onTicker);
-//  ticker.detach();
+	fRun = 0;
+	setMeasure(fRun);
 }
 
 void loop()
 {
-	readReg(I2C_ADDR_IMU0, 0x0c, buf, 6); // accxL, accxH, accyL, accyH, acczL, acczH
-	ax[0] = conv_acc(buf[1], buf[0]);
-	ay[0] = conv_acc(buf[3], buf[2]);
-	az[0] = conv_acc(buf[5], buf[4]);
-	readReg(I2C_ADDR_IMU1, 0x0c, buf, 6); // accxL, accxH, accyL, accyH, acczL, acczH
-	ax[1] = conv_acc(buf[1], buf[0]);
-	ay[1] = conv_acc(buf[3], buf[2]);
-	az[1] = conv_acc(buf[5], buf[4]);
-	printf("%f %f %f / %f %f %f\n", ax[0], ay[0], az[0], ax[1], ay[1], az[1]);
+	M5.update();
+
+	auto t = M5.Touch.getDetail();
+	if (t.isPressed()){
+		fRun = 1 - fRun;
+		setMeasure(fRun);
+		delay(500);
+	}
 }
 
